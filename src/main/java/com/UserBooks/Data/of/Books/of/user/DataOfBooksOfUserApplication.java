daced81f9c2943a7817e0d3efd8578e1e88c8ea4
Irving <sefiro2016@gmail.com>
2022-12-16 22:16:56 -0600

    initial project structure

---
+++ src/main/java/com/UserBooks/Data/of/Books/of/user/DataOfBooksOfUserApplication.java
@@ -0,0 +1,13 @@
+package com.UserBooks.Data.of.Books.of.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOfBooksOfUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataOfBooksOfUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UserBooks/Data/of/Books/of/user/DataOfBooksOfUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserBooks.Data.of.Books.of.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataOfBooksOfUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
